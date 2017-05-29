42113fdfca8dc3d8f905e115f6e2c6f0d2e0eafe
Tim Casey <timothy.e.casey@gmail.com>
2017-05-29 19:36:55 -0400

    assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,25 @@
- var pointsArray = document.getElementsByClassName('point');
+ var pointsArr = document.getElementsByClassName('point');
+
+ var revealPoint = function(point) {
+     point.style.opacity = 1;
+     point.style.transform = "scaleX(1) translateY(0)";
+     point.style.msTransform = "scaleX(1) translateY(0)";
+     point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
  
  var animatePoints = function(points) {
-
- var revealPoint = function(number) {
-         points[number].style.opacity = 1;
-         points[number].style.transform = "scaleX(1) translateY(0)";
-         points[number].style.msTransform = "scaleX(1) translateY(0)";
-         points[number].style.WebkitTransform = "scaleX(1) translateY(0)";
-}
-    
-    for (var i = 0; i < points.length; i++) {
-            revealPoint(i);
-     }
-
+     forEach(points, revealPoint);
 };
 
  window.onload = function() {
 
     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+         animatePoints(pointsArr);
      }
-
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
   window.addEventListener('scroll', function(event) {
-     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+     if (pointsArr[0].getBoundingClientRect().top <= 500) {
+             animatePoints(pointsArr);   
          }
      });
 }
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+        cb(arr[i]);
+  }
+}
