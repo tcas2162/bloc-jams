3f1171a3609c18f80d0bd97187bb52bae09aba59
Tim Casey <timothy.e.casey@gmail.com>
2017-06-25 19:31:10 -0400

    assignment 33 completed

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -5,11 +5,11 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { name: 'Blue', length: '2:16', audioUrl: 'assets/music/blue' },
-        { name: 'Green', length: '1:44', audioUrl: 'assets/music/green' },
-        { name: 'Red', length: '6:13', audioUrl: 'assets/music/red' },
-        { name: 'Pink', length: '2:32', audioUrl: 'assets/music/pink' },
-        { name: 'Magenta', length: '4:27', audioUrl: 'assets/music/magenta' }
+        { name: 'Blue', length: '161.71', audioUrl: 'assets/music/blue' },
+        { name: 'Green', length: '103.96', audioUrl: 'assets/music/green' },
+        { name: 'Red', length: '268.45', audioUrl: 'assets/music/red' },
+        { name: 'Pink', length: '153.14', audioUrl: 'assets/music/pink' },
+        { name: 'Magenta', length: '374.22', audioUrl: 'assets/music/magenta' }
     ]
 };
 
